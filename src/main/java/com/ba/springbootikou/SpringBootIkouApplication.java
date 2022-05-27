977c4eef12d0240d977c63ebf5d0b2444dd7faa5
asifmalik-ba <asif.salman.malik@b-architects.com>
2022-05-27 14:48:16 +0900

    First Commit for Spring Boot Application

---
+++ src/main/java/com/ba/springbootikou/SpringBootIkouApplication.java
@@ -0,0 +1,13 @@
+package com.ba.springbootikou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIkouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIkouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ba/springbootikou/SpringBootIkouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ba.springbootikou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIkouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
