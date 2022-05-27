7577a1c31835d395e0a4713ec75ac113960a09e1
asifmalik-ba <asif.salman.malik@b-architects.com>
2022-05-27 16:22:29 +0900

    TopicService Class Completion

--- src/main/java/com/ba/springbootikou/topic/TopicController.java
+++ src/main/java/com/ba/springbootikou/topic/TopicController.java
@@ -2,7 +2,6 @@ package com.ba.springbootikou.topic;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
--- src/main/java/com/ba/springbootikou/topic/TopicService.java
+++ src/main/java/com/ba/springbootikou/topic/TopicService.java
@@ -1,5 +1,21 @@
 package com.ba.springbootikou.topic;
 
+import java.util.Arrays;
+
+
+import java.util.List;
+
+//16:16
 public class TopicService {
     
+    private List<Topic> topics = Arrays.asList(
+        new Topic("java-01", "Java Introduction", "Hands-on JavaScript to learn from scratch."),
+        new Topic("spring-01", "Spring Boot Course", "Spring Boot for beginners."),
+        new Topic("js-01", "Basic JavaScript", "Basic JavaScript course for Students.")
+    );
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
 }
