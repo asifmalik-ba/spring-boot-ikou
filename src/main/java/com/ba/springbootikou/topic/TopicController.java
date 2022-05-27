26631b526c85c1bc380cb76fc5c127bb50fd887a
asifmalik-ba <asif.salman.malik@b-architects.com>
2022-05-27 16:09:43 +0900

    TopicController Setup & Testing

--- src/main/java/com/ba/springbootikou/topic/TopicController.java
+++ src/main/java/com/ba/springbootikou/topic/TopicController.java
@@ -1,5 +1,16 @@
 package com.ba.springbootikou.topic;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class TopicController {
+    
+    @GetMapping("/topic")
+    public String getMethodName() {
+        return "This is Topic Controller";
+    }
     
 }
